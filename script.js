836759d39bae03a9ab01259b339dc057dfa0d00a
Kim Morgan <101612445+kcpmorgan@users.noreply.github.com>
2022-04-11 12:10:03 -0700

    Added CSS

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ function add(){
     showTasks();
 }
 
+
 function showTasks(){
     let ullist = document.getElementById(`todoList`);
     ullist.innerHTML = "";
     let bodyList = "";
     
     for(let i=0; i<todolist.length; i++){
-        bodyList += `<li> ${todolist[i]} </li>`;
+        bodyList += `<li> ${todolist[i]}  <button onclick="remove(${i})">remove</button></li> `;
     }
 
     ullist.innerHTML = bodyList;
+
+}
+
+function remove(index){
+    todolist.splice(index,1);
+    showTasks();
 }
